ef6015beb69315df99fa6f2b307e2fd415c084d2
Silvio Traversaro <silvio@traversaro.it>
2024-08-20 18:53:34 +0200

    Create run_test.bat

---
+++ recipe/run_test.bat
@@ -0,0 +1,10 @@
+setlocal EnableDelayedExpansion
+
+cd cpp\minimal
+
+:: Compile example that links rerun
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release .
+if errorlevel 1 exit 1
+
+cmake --build . --config Release
+if errorlevel 1 exit 1
