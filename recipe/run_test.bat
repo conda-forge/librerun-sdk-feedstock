56eef3a669fe1c236c420551b301de3fbdaa7d57
Silvio Traversaro <silvio@traversaro.it>
2024-08-20 19:31:55 +0200

    Update run_test.bat

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -3,7 +3,7 @@ setlocal EnableDelayedExpansion
 cd examples\cpp\minimal
 
 :: Compile example that links rerun
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release .
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DRERUN_FIND_PACKAGE:BOOL=ON .
 if errorlevel 1 exit 1
 
 cmake --build . --config Release
