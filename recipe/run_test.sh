a287a49cb20dea40b58ca6ba4d7de8be8ab94f8f
Silvio Traversaro <silvio@traversaro.it>
2025-11-16 21:58:19 +0100

    Update run_test.sh to include CXXFLAGS
    Add CXXFLAGS to disable availability warnings.

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+# See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+# We pass this also on non-macos as it is harmless
+CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+
 # Test linking against the rerun library
 cd examples/cpp/minimal
 
